539d4474a21c40b6149c61b836fdccc4cf1505b8
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-07-29 15:19:32 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const burgerMenu = document.querySelector('.header__burger-menu');
+const headerLanguage = document.querySelector('.header__language');
+const menu = document.querySelector('.header__hidden-view');
+const body = document.body;
+
+burgerMenu.addEventListener('click', () => {
+  menu.classList.toggle('header__hidden-view--active');
+
+  body.classList.toggle('no-scroll');
+
+  burgerMenu.classList.toggle('header__burger-menu--active');
+
+  headerLanguage.classList.toggle('header__language--active');
+});
